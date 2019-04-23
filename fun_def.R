5b2dfb06437eded3709361bfd5476fb3f8fb1aae
Hadley Wickham <h.wickham@gmail.com>
2019-04-23 09:11:55 -0500

    Polishing magical defaults

--- fun_def.R
+++ fun_def.R
@@ -9,18 +9,45 @@ pkg_funs <- function(pkg) {
   map2(names(funs), funs, fun_def, pkg = pkg)
 }
 
-fun_def <- function(name, fun, pkg = NULL) {
+fun_call <- function(fun, ...) {
+  call <- enexpr(fun)
+  
+  if (is.symbol(call)) {
+    name <- as.character(call)
+    env_name <- env_name(environment(fun))
+    pkg <- if (grepl("namespace:", env_name)) gsub("namespace:", "", env_name) else NULL 
+  } else if (is_call(call, "::", n = 2)) {
+    name <- as.character(call[[3]])
+    pkg <- as.character(call[[2]])
+  } else {
+    abort("Invalid input")
+  }
+  
+  fun_def(name, fun, pkg = pkg, ...)
+}
+
+fun_def <- function(name, fun, pkg = NULL, highlight = NULL) {
+  stopifnot(is_string(name))
+  stopifnot(is.function(fun))
+  
   new_fun_def(
     name = name,
     formals = formals(fun),
     body = body(fun),
-    pkg = pkg
+    pkg = pkg,
+    highlight = highlight
   )
 }
 
-new_fun_def <- function(name, formals, body, pkg = NULL) {
+new_fun_def <- function(name, formals, body, pkg = NULL, highlight = NULL) {
   structure(
-    list(name = name, formals = formals, body = body, pkg = pkg),
+    list(
+      name = name, 
+      formals = formals, 
+      body = body, 
+      pkg = pkg,
+      highlight = highlight
+    ),
     class = "fun_def"
   )
 }
@@ -37,6 +64,12 @@ format.fun_def <- function(x, ...) {
     is_missing <- map_lgl(formals, is_missing)
     formals[is_missing] <- syms(names(formals)[is_missing])
     names(formals)[is_missing] <- ""
+  }
+  
+  # Doesn't work because format escapes  
+  if (!is.null(x$highlight)) {
+    embold <- names(formals) %in% x$highlight
+    names(formals)[embold] <- cli::style_bold(names(formals)[embold])
   }
   
   paste0(format(call2(call, !!!formals)), collapse = "\n")
