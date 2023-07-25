07457aff5d5d5f38b2a0540480d83c3c7658fc0c
Hadley Wickham <h.wickham@gmail.com>
2023-07-25 08:36:48 -0500

    Write rename funciton
    Fixes #151

--- common.R
+++ common.R
@@ -7,3 +7,23 @@ options(
   rlang_trace_top_env = rlang::current_env(),
   rlang_backtrace_on_error = "none"
 )
+
+rename <- function(old, new) {
+  old_path <- fs::path_ext_set(old, "qmd")
+  new_path <- fs::path_ext_set(new, "qmd")
+  
+  fs::file_move(old_path, new_path)
+  quarto <- readLines("_quarto.yml")
+  quarto <- gsub(old_path, new_path, quarto, fixed = TRUE)
+  writeLines(quarto, "_quarto.yml")
+
+  old_slug <- paste0("sec-", old)
+  new_slug <- paste0("sec-", new)
+  
+  qmd_paths <- dir(pattern = ".qmd$")
+  qmds <- lapply(qmd_paths, readLines)
+  qmds <- lapply(qmds, \(lines) gsub(old_slug, new_slug, lines, fixed = TRUE))
+  purrr:::map2(qmds, qmd_paths, \(lines, path) writeLines(lines, path))
+  
+  invisible()
+}
